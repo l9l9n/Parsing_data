72b96ae3906eabff3ecee5b8609654d39bc13d63
l9l9n <l9l9ndos@gmail.com>
2023-07-31 21:02:46 +0600

    Refactoring code

--- main.py
+++ main.py
@@ -4,10 +4,26 @@ from bs4 import BeautifulSoup as BS
 
 URL = "https://www.house.kg/snyat-kvartiru?region=1&town=2&sort_by=upped_at+desc"
 
-response = requests.get(URL)
-html = response.text
-# print(html, 'html.parser')
+def get_html(url):
+    response = requests.get(URL)
+    # html = response.text
+    # print(html, 'html.parser')
+    if response.status_code == 200:
+        return response.text
+    return None
 
 soup = BS(html, "html.parser")
 container = soup.find("div", {"class": "container body-container"})
-print(container)
+main = container.find("div", {"class": "main-content"})
+listing = main.find("div", {"class": "listings-wrapper"})
+posts = listing.find_all("div", {"class": "listing"})
+for post in posts:
+    header = post.find("div", {"class": "left-side"})
+    title = header.find("p", {"class": "title"}).text.strip()
+    address = header.find("div", {"class": "address"}).text.strip()
+    link = header.find("a").get("href")
+    full_link = 'https://www.house.kg'+link
+    price_dol = post.find("div", {"class": "sep main"}).find("div", {"class": "price"}).text.strip()
+    price_som = post.find("div", {"class": "sep main"}).find("div", {"class": "price-addition"}).text.strip()
+    desk = post.find("div", {"class": "description"}).text.strip()
+    print(desk)

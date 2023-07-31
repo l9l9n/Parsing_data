e48f32e6b2d08c4be2564ce6c700e567d4d4e523
l9l9n <l9l9ndos@gmail.com>
2023-07-31 19:59:12 +0600

    Start parsing

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 
+URL = "https://www.house.kg/snyat-kvartiru?region=1&town=2&sort_by=upped_at+desc"
 
-file = open('test.html', encoding='utf-8')
+response = requests.get(URL)
+html = response.text
+# print(html, 'html.parser')
 
-html = file.read()
-soup = BS(html, 'html.parser')
-
-container = soup.find('div', {"class": "container"}).find('div', {"class": "navigation-container"})
-ul_list_container = container.find("ul", {"class": "menu"})
-li_list = ul_list_container.find_all('li')
-
-for li in li_list:
-    print(li.get_text())
+soup = BS(html, "html.parser")
+container = soup.find("div", {"class": "container body-container"})
+print(container)
